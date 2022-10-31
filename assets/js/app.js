229bda293e92537d2e34b1c39a38d006ab559ab2
DaniSanchezG <laughingoutloud90@gmail.com>
2022-10-31 17:26:22 +0100

    Gallery scroll done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,22 @@
-let button1 = document.querySelector('#botonPruebaScrollLeft');
-let button2 = document.querySelector('#botonPruebaScrollRight');
-let galery = document.querySelector('.container-gallery');
+let button1 = document.querySelector("#botonPruebaScrollLeft");
+let button2 = document.querySelector("#botonPruebaScrollRight");
+let galery = document.querySelector(".container-gallery");
 let galeryMovement = 0;
-console.log(galery);
 
-button2.addEventListener('click', () => {
+button2.addEventListener("click", () => {
+  if (galeryMovement < 7020) {
     galery.scrollTo({
-        left: galeryMovement += 540,
-        behavior: 'smooth'
+      left: (galeryMovement += 540),
+      behavior: "smooth",
     });
+  }
 });
 
-button1.addEventListener('click', () => {
+button1.addEventListener("click", () => {
+  if (galeryMovement > 0) {
     galery.scrollTo({
-        left: galeryMovement -= 540,
-        behavior: 'smooth'
+      left: (galeryMovement -= 540),
+      behavior: "smooth",
     });
+  }
 });
